cd19d3d8f134806c8d5ecd6273fa9669e141d29e
Fee-ing <2809750987@qq.com>
2017-12-01 11:12:05 +0800

    修复小bug

--- src/views/login.vue
+++ src/views/login.vue
@@ -55,7 +55,7 @@ export default {
       };
       document.onkeydown = function(event) {
         var e = event || window.event || arguments.callee.caller.arguments[0];
-        if (e && e.keyCode == 13) {
+        if (e && e.keyCode == 13 && that.$route.path === '/login') {
           that.submitOpt();
         }
       };
