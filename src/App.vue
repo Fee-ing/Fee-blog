6c3e65e6f9691c10ea8eee33692b0302bede688e
Fee-ing <2809750987@qq.com>
2018-08-29 11:44:08 +0800

    添加过渡动画

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
   <div id="app">
     <div id="toast"></div>
-    <router-view/>
+    <transition :name="transitionName">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      transitionName: ''
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      const toDepth = (to.path.split('/')).filter(ele => { return ele }).length
+      const fromDepth = (from.path.split('/')).filter(ele => { return ele }).length
+      this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+    }
+  }
 }
 </script>
+
+<style lang="less" scoped>
+.slide-right-enter-active,
+.slide-right-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active {
+  will-change: transform;
+  transition: all .3s;
+  position: absolute;
+  width:100%;
+  left:0;
+}
+.slide-right-enter {
+  transform: translateX(-100%);
+}
+.slide-right-leave-active {
+  transform: translateX(100%);
+}
+.slide-left-enter {
+  transform: translateX(100%);
+}
+.slide-left-leave-active {
+  transform: translateX(-100%);
+}
+</style>
